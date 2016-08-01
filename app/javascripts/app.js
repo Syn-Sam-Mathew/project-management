07df1ef713a33fd8dab2c0c13a1c1dddc08a16ad
Sam <smath045@fiu.edu>
2016-08-01 14:34:42 -0400

    Added Active/Completed Project Views

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -7,19 +7,20 @@ app.controller("MainController",['$scope',function($scope){
   $scope.projects = [];
   $scope.project = {};
   $scope.asset = "";
+  $scope.reject = "";
+  $scope.done = "";
   var accounts = web3.eth.accounts;
   web3.eth.defaultAccount = accounts[0];
-  $scope.selectedAccount = accounts[0];
+  $scope.selectedAccount = accounts[1];
   var ethPMA = EthPMA.at(EthPMA.deployed_address);
   var myethPMA = ethPMA;
 
   $scope.accounts={
-    "Client" : accounts[0],
-    "Synechron" : accounts[1],
-    "Project Manager" : accounts[2],
-    "Developer" : accounts[3] 
+    "Client" : accounts[1],
+    "Synechron" : accounts[2],
+    "Project Manager" : accounts[3],
+    "Developer" : accounts[4] 
   }
-
 
   EthPMA.new().then(
     function(conf) {
@@ -30,17 +31,41 @@ app.controller("MainController",['$scope',function($scope){
       myethPMA.Update().watch(function(error, event) {
         if (!error) {
           $scope.updatePending(event.args);
+          $scope.showBalance();
           console.log('projects', $scope.projects);
         } else {
           console.log('error occurred', err);
         }
       });
-
-
+      myethPMA.Ended().watch(function(error, event) {
+        if (!error) {
+          console.log('project ended', event);
+          $scope.showBalance();
+        } else {
+          console.log('error occurred', err);
+        }
+      });
+      $scope.showBalance();
       console.log('EthPMA created');
     }, function(err) {
       console.log(err);
     });
+
+  $scope.showBalance = function(){
+  var balance=web3.eth.getBalance(ethPMA.address);
+  $scope.balance = web3.fromWei(balance.toNumber(), 'ether');
+  $scope.balances={
+    "Client" : web3.fromWei(web3.eth.getBalance(accounts[1]).toNumber(),"ether"),
+    "Synechron" : web3.fromWei(web3.eth.getBalance(accounts[2]).toNumber(),"ether"),
+    "Project Manager" : web3.fromWei(web3.eth.getBalance(accounts[3]).toNumber(),"ether"),
+    "Developer" : web3.fromWei(web3.eth.getBalance(accounts[4]).toNumber(),"ether")
+  }
+  console.log($scope.balances.Client);
+  console.log($scope.accounts.Client);
+  console.log($scope.balances.Synechron);
+  console.log($scope.accounts.Synechron);
+  $scope.$apply();
+  }
 
   $scope.startRFP = function(){
     //console.log($scope.project);
@@ -54,31 +79,47 @@ app.controller("MainController",['$scope',function($scope){
     $scope.status = $scope.serialize($scope.project.status);
     $scope.assets = $scope.serialize($scope.project.assets);
     //console.log($scope.status);
-    //console.log($scope.assignTo);
-    myethPMA.startRFP($scope.project.name, $scope.project.description, $scope.project.amount, $scope.selectedAccount, $scope.assignTo, $scope.status, $scope.assets).then (
+    console.log($scope.assignTo);
+    var xvalue = web3.toWei(Number($scope.project.amount),"ether");
+    var xfrom = $scope.selectedAccount;
+    console.log(xfrom);
+    console.log(xvalue);
+    myethPMA.startRFP($scope.project.name, $scope.project.description, $scope.accounts.Synechron,$scope.assignTo, $scope.status, $scope.assets, { from: xfrom, value: xvalue }).then (
     function (project)
     { 
       $scope.project= {}; 
       console.log('Action created: ', project);
+      console.log('Update done')
     });
   }
 
   $scope.updatePending = function(data){
+    var count = 0;
     $scope.projects[data.projectID.toNumber()] = {}
     $scope.projects[data.projectID.toNumber()].projectID= data.projectID.toNumber();
     $scope.projects[data.projectID.toNumber()].name= data.name;
     $scope.projects[data.projectID.toNumber()].description = data.description
-    $scope.projects[data.projectID.toNumber()].amount = data.amount;
-    $scope.projects[data.projectID.toNumber()].recipient = data.recipient;
+    $scope.projects[data.projectID.toNumber()].amount = data.amount.toNumber();
+    $scope.projects[data.projectID.toNumber()].client = data.client;
+    $scope.projects[data.projectID.toNumber()].consultant = data.consultant;
     $scope.projects[data.projectID.toNumber()].assignTo = [];
     $scope.projects[data.projectID.toNumber()].assignTo = data.assignTo.split(",");
     $scope.projects[data.projectID.toNumber()].status = data.status.split(",");
     $scope.projects[data.projectID.toNumber()].assets = data.assets.split(",");
     $scope.projects[data.projectID.toNumber()].phase = {};
     $scope.projects[data.projectID.toNumber()].projectAsset = {};
+    $scope.projects[data.projectID.toNumber()].reject = "";
+    $scope.projects[data.projectID.toNumber()].done = "";
     for (var i = 0 ; i < $scope.projects[data.projectID.toNumber()].assignTo.length ; i++){
       $scope.projects[data.projectID.toNumber()].phase[$scope.getAccountName($scope.projects[data.projectID.toNumber()].assignTo[i])] = $scope.projects[data.projectID.toNumber()].status[i];
-      $scope.projects[data.projectID.toNumber()].projectAsset[$scope.getAccountName($scope.projects[data.projectID.toNumber()].assignTo[i])] = $scope.projects[data.projectID.toNumber()].assets[i];  
+      $scope.projects[data.projectID.toNumber()].projectAsset[$scope.getAccountName($scope.projects[data.projectID.toNumber()].assignTo[i])] = $scope.projects[data.projectID.toNumber()].assets[i];
+      if ($scope.projects[data.projectID.toNumber()].status[i] == "true"){
+        count = count + 1;
+      }  
+    }
+    if(count == $scope.projects[data.projectID.toNumber()].assignTo.length)
+    {
+      $scope.endContract(data.name);
     }
     console.log($scope.projects[data.projectID.toNumber()].phase);
     console.log($scope.projects[data.projectID.toNumber()].projectAsset);
@@ -92,6 +133,29 @@ app.controller("MainController",['$scope',function($scope){
           return key;
         }
       }
+  }
+
+  $scope.endContract = function(name)
+  {
+    var id = $scope.getId(name);
+    $scope.projects[id].done = "true";
+    console.log(id);
+    console.log($scope.projects[id].done);
+    myethPMA.endContract(id, {from : $scope.projects[id].consultant}).then(
+      function (project)
+    { 
+      console.log('Contract Ended: ', project);
+    });
+  }
+
+  $scope.reject = function(name){
+    var id = $scope.getId(name);
+    $scope.projects[id].reject = "true";
+    myethPMA.rejectContract(id, {from : $scope.projects[id].client}).then(
+      function (project)
+    { 
+      console.log('Contract Ended: ', project);
+    });
   }
 
   $scope.accept = function(name,value,asset){
--- contracts/EthPMA.sol
+++ contracts/EthPMA.sol
@@ -3,7 +3,8 @@ contract EthPMA{
 		string name;
 		string description;
 		uint amount;
-		address recipient;
+		address client;
+		address consultant;
 		string assignTo;
 		string status;
 		string assets;
@@ -11,24 +12,59 @@ contract EthPMA{
 	mapping(uint => project) Projects;
 	uint totalProjects;
 
-	event Update(string name, string description, uint amount, uint projectID, address recipient, string assignTo, string status, string assets);
+	event Update(string name, string description, uint amount, uint projectID, address client, address consultant,string assignTo, string status, string assets);
+	event Ended(uint projectID);
 
-	function startRFP(string name, string description, uint amount, address recipient, string assignTo,string status, string assets) returns (uint projectID){
-		projectID = totalProjects++;
+	function startRFP(string name, string description, address consultant,string assignTo,string status, string assets)  {
+		uint projectID = totalProjects++;
+
 		Projects[projectID].name = name;
 		Projects[projectID].description = description;
-		Projects[projectID].amount = amount;
-		Projects[projectID].recipient = recipient;
+		Projects[projectID].amount = msg.value;
+		Projects[projectID].client = msg.sender;
+		Projects[projectID].consultant = consultant;
 		Projects[projectID].assignTo = assignTo;
 		Projects[projectID].status = status;
 		Projects[projectID].assets = assets;
-		Update(name, description, amount, projectID, recipient, assignTo, status, assets); 
+		//Projects[projectID].client.send(Projects[projectID].amount);
+		
+
+		/*uint projectID = 0;
+
+		Projects[projectID].name = name;
+		Projects[projectID].description = description;
+		Projects[projectID].amount = msg.value;
+		Projects[projectID].client = msg.sender;
+		Projects[projectID].consultant = consultant;
+		Projects[projectID].assignTo = assignTo;
+		Projects[projectID].status = status;
+		Projects[projectID].assets = assets;
+		
+		//Projects[projectID].client.send(Projects[projectID].amount);
+		*/
+
+
+		Update(Projects[projectID].name, Projects[projectID].description, Projects[projectID].amount, projectID, Projects[projectID].client, Projects[projectID].consultant, Projects[projectID].assignTo, Projects[projectID].status, Projects[projectID].assets); 
 	}
 
 	function updateContract(uint id,string status, string assets){
 		project p = Projects[id];
 		p.status = status;
 		p.assets = assets;
-		Update(p.name, p.description, p.amount, id, p.recipient, p.assignTo, p.status, p.assets);	
+		Update(p.name, p.description, p.amount, id, p.client, p.consultant, p.assignTo, p.status, p.assets);	
+	}
+
+	function endContract(uint id){
+		project p = Projects[id];
+		p.consultant = msg.sender;
+		p.consultant.send(10000000000000000000000);
+		Ended(id);
+	}
+
+	function rejectContract(uint id){
+		project p = Projects[id];
+		p.client = msg.sender;
+		p.client.send(p.amount);
+		Ended(id);
 	}
 }
